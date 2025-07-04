1c2492d5a76fff88c694291041f253fa7073c3c4
Onur Kahan <onurkahan8@gmail.com>
2025-07-04 10:52:18 +0300

    day2

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.listen(3000)
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     // res.send('<h1>Main Menu</h1>')
     res.render('index', { title: 'Main Menu' })
 })
+
 
 app.get('/about', (req, res) => {
     // res.send('<h1>About</h1>')
